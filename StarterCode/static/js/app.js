00543dfaf0754ca6fbb6e7406fcb8aa8604e226d
Kylmalcolm <kylie.a.malcolm@gmail.com>
2021-01-16 21:11:11 -0600

    working on selection & metadata

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,7 @@ d3.json("./static/js/samples.json").then((sampleData) => {
     var names = sampleData.names;
     console.log(names);
 
-    // line 25 of index.html for dropdown
+    // ID in line 25 of index.html for dropdown
     var selectTag = d3.select("#selDataset");
 
     names.forEach((person) => {
@@ -13,3 +13,24 @@ d3.json("./static/js/samples.json").then((sampleData) => {
     });
 });
 
+function optionChanged(selectedPerson) {
+    // grab the value of the input field
+    var inputText = d3.event.target.value;
+  
+    // reverse the input string
+    var reversedInput = reverseString(inputText);
+  
+    // Set the output text to the reversed input string
+    output.text(reversedInput);
+  }
+  
+  text.on("change", optionChanged);
+
+function filterResults(person) {
+    return sampleData.metadata.id === person;
+  }
+  
+//   var filteredMovies = topMovies.filter(filterMovieRatings);
+
+// <h3 class="panel-title">Demographic Info</h3>
+{/* <div id="sample-metadata" class="panel-body"></div> */}
